d51e81b6f0a4dd01e88fe8ea6d6950e2b2f6bf67
Valentim Araújo <valentim@evodev.com.br>
2023-01-05 11:10:56 -0300

    feat: lambda stream listener

--- bookings-consumer/src/functions/stream-listener/handler.ts
+++ bookings-consumer/src/functions/stream-listener/handler.ts
@@ -1,11 +1,20 @@
 import { formatJSONResponse } from '@libs/api-gateway';
 import * as AWS from 'aws-sdk';
+import moment from 'moment';
+import 'moment/locale/pt-br'
 
 const convert = AWS.DynamoDB.Converter;
+moment.locale('pt-br')
 
 const streamListener = async (event) => {
   for (const record of event.Records) {
-    console.log('JSON parse', convert.unmarshall(record.dynamodb.NewImage))
+    if (record.eventName === 'INSERT') {
+      const booking = convert.unmarshall(record.dynamodb.NewImage);
+      console.log(
+          `Reserva efetuada: o cliente ${booking.user.name} (${booking.user.email}) ` +
+          `agendou um horário em: ${moment.unix(booking.date).format('LLLL')}`
+      )
+    }
   }
   return formatJSONResponse({
     message: `stream listener succefully!`,
