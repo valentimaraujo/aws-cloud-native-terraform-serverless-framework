6c0418b3b2a77468628d38188daae473b2f16545
Valentim Araújo <valentim@evodev.com.br>
2023-01-06 07:54:32 -0300

    feat: sns notifications subscription

---
+++ terraform/infra/notifications/iam-policy-email.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_policy" "bookings_stream_consumer_policy" {
+  name = "${var.environment}-bookings-stream-consumer-policy"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "dynamodb:ListStreams",
+          "dynamodb:DescribeStream",
+          "dynamodb:GetShardIterator",
+          "dynamodb:GetRecords",
+        ],
+        "Effect" : "Allow",
+        "Resource" : "${aws_dynamodb_table.bookings.stream_arn}"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "sns:Publish"
+        ],
+        "Resource" : var.sns_notifications_arn
+      }
+    ]
+  })
+}
